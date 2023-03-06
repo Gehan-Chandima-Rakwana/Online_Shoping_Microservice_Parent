9272ed7312da1b6e645fd38124bf26a4b3b130a0
Gehan_Chandima_Rakwana <grakwana@gmail.com>
2023-03-06 21:54:29 +0530

    spring cloud sleuth and zipkin implementation

--- order_service/src/main/java/com/example/orderservice/serviceImpl/OrderServiceImpl.java
+++ order_service/src/main/java/com/example/orderservice/serviceImpl/OrderServiceImpl.java
@@ -9,6 +9,8 @@ import com.example.orderservice.repository.OrderRepository;
 import com.example.orderservice.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -27,19 +29,22 @@ public class OrderServiceImpl implements OrderService {
 
     private final WebClient webClient; //already created in config bean
 
+    private final Tracer tracer;// This is from spring cloud sleuth for declare our own span ID
+
     @Autowired
     private WebClient.Builder webClientBuilder;
 
 
     @Autowired
-    public OrderServiceImpl(OrderRepository orderRepository, WebClient webClient) {
+    public OrderServiceImpl(OrderRepository orderRepository, WebClient webClient, Tracer tracer) {
         this.orderRepository = orderRepository;
         this.webClient = webClient;
+        this.tracer = tracer;
     }
 
 
     @Override
-    public void placeOrder(OrderRequestDto orderRequestDto) {
+    public String placeOrder(OrderRequestDto orderRequestDto) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
@@ -54,20 +59,52 @@ public class OrderServiceImpl implements OrderService {
                 .map(OrderLineItems::getSkuCode)
                 .collect(Collectors.toList());
 
-        //Need to call inventory service and need to check the availability of stock
-        InventoryResponseDto[] inventoryResponseArray = callInventoryService(skuCodes);
+//        //Need to call inventory service and need to check the availability of stock
+//        InventoryResponseDto[] inventoryResponseArray = callInventoryService(skuCodes);
+//
+//        //by default web client make asynchronous request
+//        //there for we use .block() to make it as synchronous
+//        //synchronous mean it will waite until response come to execute other codes.
+//
+//        boolean allProductInStock = Arrays.stream(inventoryResponseArray)
+//                .allMatch(InventoryResponseDto::isInStock);
+//
+//        if(allProductInStock) {
+//            orderRepository.save(order);
+//            return "Success";
+//        }else {
+//            throw new IllegalArgumentException("Product is not in stock");
+//        }
 
-        //by default web client make asynchronous request
-        //there for we use .block() to make it as synchronous
-        //synchronous mean it will waite until response come to execute other codes.
+//        Spring cloud sleuth provide us create our own spam ID
+//        So In here try to create span id for inventory service
+//        Above code is without declare our own span ID [line 61 to 78]
+//        Bellow code is with our own span id line [88 to 103]
+//        Both are same logic
 
-        boolean allProductInStock = Arrays.stream(inventoryResponseArray)
-                .allMatch(InventoryResponseDto::isInStock);
+        Span inventoryServiceLookUp = tracer.nextSpan().name("InventoryServiceLookUp");
 
-        if(allProductInStock)
-            orderRepository.save(order);
-        else
-            throw new IllegalArgumentException("Product is not in stock");
+        try(Tracer.SpanInScope spanInScope = tracer.withSpan(inventoryServiceLookUp.start())){
+            //Need to call inventory service and need to check the availability of stock
+            InventoryResponseDto[] inventoryResponseArray = callInventoryService(skuCodes);
+
+            //by default web client make asynchronous request
+            //there for we use .block() to make it as synchronous
+            //synchronous mean it will waite until response come to execute other codes.
+
+            boolean allProductInStock = Arrays.stream(inventoryResponseArray)
+                    .allMatch(InventoryResponseDto::isInStock);
+
+            if(allProductInStock) {
+                orderRepository.save(order);
+                return "Success";
+            }else {
+                throw new IllegalArgumentException("Product is not in stock");
+            }
+
+        }finally {
+            inventoryServiceLookUp.end();
+        }
     }
 
     private InventoryResponseDto[] callInventoryService(List<String> skuCodes) {
